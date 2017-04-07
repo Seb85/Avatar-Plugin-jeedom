6c45aadffc89e627f7452efd55ae77d2a0fb27f7
Seb85 <sebrenaud@hotmail.fr>
2017-04-07 17:53:15 +0200

    Update intent.jeedom.js

--- intents/intent.jeedom.js
+++ intents/intent.jeedom.js
@@ -7,16 +7,20 @@ Object.defineProperty(exports, "__esModule", {
 var _helpers = require('../../node_modules/ava-ia/lib/helpers');
 
 exports.default = function (state, actions) {
- 
+	  
 	if (state.isIntent) return (0, _helpers.resolve)(state);
-	
-	var tokens = (0, _helpers.intersect)(Config.modules.jeedom.rules, state.tokens);
-  
-	if (tokens) {
+	 
+	for (var rule in Config.modules.jeedom.rules) {
+		var match = (0, _helpers.syntax)(state.sentence, Config.modules.jeedom.rules[rule]); 
+		if (match) break;
+	}
+
+	if (match) {
+		if (state.debug) info('IntentJeedom'.bold.green, 'syntax:',((match) ? 'true'.green : 'false'.green));
 		state.isIntent = true;
-		if (state.debug) info('IntentJeedom'.bold.green, 'tokens:', tokens.toString().green);
 		return (0, _helpers.factoryActions)(state, actions);
 	} else 
 		return (0, _helpers.resolve)(state);
 	
+  
 };
