e45cd1c06b525d51c4dc9750bda33b8f5887d058
Seb85 <sebrenaud@hotmail.fr>
2017-04-11 17:57:22 +0200

    Update jeedom.js

--- jeedom/jeedom.js
+++ jeedom/jeedom.js
@@ -1,138 +1,108 @@
-var Promise = require('q').Promise;
 var request = require('request');
 
 require('colors');
 
-exports.action = function (data, callback) {
-
-    info("Jeedom from:", data.client.yellow);
-    var debug = false;
-
-    /************************************************************************************************
-     ** require list
-     ************************************************************************************************/
-    var EventEmitter = require('events').EventEmitter; //
-
-    /************************************************************************************************
-     ** Path list
-     ************************************************************************************************/
-    var pathJeedomApi = '/core/api/jeeApi.php';
-
-    /************************************************************************************************
-     ** callbackReturn method
-     ************************************************************************************************/
-    var callbackReturn = new EventEmitter();
-
-    /************************************************************************************************
-     ** jeedomProcess method
-     ************************************************************************************************/
-    var jeedomProcess = new EventEmitter();
-
-    /***************************************************
-     ** @description Send interaction request to jeedom
-     ** @function execute
-     ***************************************************/
-    jeedomProcess.on('execute', function () {
-        console.log('--------EXECUTE--------');
-        var jsonrpc = getJsonRpc();
-        jsonrpc.method = 'execute';
-        for (var i in data) {
-            jsonrpc.params.id = data.action.id;
-        }
-        sendJsonRequest(jsonrpc);
-    });
+exports.init = function(){
 	
-	/***************************************************
-     ** @description sendJsonRequest
-     ** @function sendJsonRequest
-     ***************************************************/
-
-	function sendJsonRequest(_jsonrpc, callback){
-        var adresse = Config.modules.jeedom.addrJeedom;
-        if (adresse.indexOf('http://') < 0) {
-            adresse = 'http://' + adresse;
-        }
-        console.log('Adresse : ' + adresse + pathJeedomApi);
-		console.log(_jsonrpc)
-        var request = require('request');
-        request({
-            url: adresse + pathJeedomApi,
-            method: 'POST',
-            form: {request: JSON.stringify(_jsonrpc)}
-        },
-        function (err, response, json) {
-            if (err || response.statusCode != 200) {
-                console.log('Error: Callback request');
-                callbackReturn.emit('tts', 'Echec de la requete à jeedom');
-            }
-            console.log('-------REQUEST SUCCESS-------');
-        });
-    }
-
-
-    /************************************************************************************************
-     ** function
-     ************************************************************************************************/
-	 
-    /***************************************************
-     ** @description Generate json rpc for jeedom
-     ** @function getJsonRpc
-     ** @return string json
-     ***************************************************/
-    function getJsonRpc() {
-        var jsonrpc = {};
-        jsonrpc.id = data.action.id;
-        jsonrpc.params = {};
-        jsonrpc.params.apikey = Config.modules.jeedom.apikeyJeedom;
-        jsonrpc.params.plugin = 'sarah';
-        jsonrpc.jsonrpc = '2.0';
-        return jsonrpc;
-    }
+	// table of properties
+	 _JeedomConf = {
+		pathJeedomApi:  Config.modules.Jeedom.php || '',
+		apikey: Config.modules.Jeedom.apikey || '',
+		ip : Config.modules.Jeedom.addrJeedom || ''
+	};
 	
-    /***************************************************
-     ** @description Update xml file from jeedom
-     ** @function readReturn
-     ** @param string _return
-     ***************************************************/
-    function readReturn(_return) {
-        callbackReturn.emit('tts', _return);
-    }
+}
+  
 
-    /***************************************************
-     ** @description ?
-     ** @function isset
-     ** @return ?
-     ***************************************************/
-    function isset() {
-        var a = arguments,
-                l = a.length,
-                i = 0,
-                undef;
-
-        if (l === 0) {
-            throw new Error('Empty isset');
-        }
-        while (i !== l) {
-            if (a[i] === undef || a[i] === null) {
-                return false;
-            }
-            i++;
-        }
-        return true;
-    }
-
-    /************************************************************************************************
-     ** Main
-     ************************************************************************************************/
-    config = Config.modules.jeedom;
-	if (!config.apikeyJeedom){
-		console.log("Clef api manquante");
-		callback({'tts' : 'Clef api manquante'});
-		return;
+  
+exports.action = function(data, callback){
+	
+	if (!_JeedomConf.pathJeedomApi || !_JeedomConf.apikey || !_JeedomConf.ip ) {
+		error("Jeedom:", "La configuration de Jeedom est manquante".red);
+		return callback();
 	}
 	
-	if(data.action.method == 'execute')
-		jeedomProcess.emit(data.action.method);
-	
-	callback();
+	var room;
+	var tblCommand = {
+		// Allume/éteint par pièce
+		switchLight : function() { 
+		    // room: la pièce où l'ordre est exécutée
+			// data.client: là où l'action a été exécutée
+			switchLight(data.client, Config.modules.Jeedom.clients[room][data.action.value]);
+		},
+		resetSwitch : function(){
+			// test avec une règle "occupe-toi de la lumière" dans "rule" du prop
+			Avatar.speak("ok, je m'occupe de la lumière dans la pièce " + room, data.client, function() {
+				Avatar.Speech.end(data.client);
+			});
+		}
 	};
+	
+	info("Jeedom command:", data.action.command.yellow, "From:", data.client.yellow);
+	room = ((data.action.room && data.action.room != 'current') ? data.action.room : data.client).toLowerCase();
+
+	tblCommand[data.action.command]();
+	callback();
+}
+
+
+
+
+function switchLight (client, value) {
+	
+	var jsonrpc = getJsonRpc(value);
+	jsonrpc.method = 'execute';
+	jsonrpc.params.id = value;
+	
+	sendJsonRequest(jsonrpc, function(state) { 
+	
+		var answer = !state ? "je ne suis pas arrivé à exécuter l'action" : Config.modules.Jeedom.answers;
+		
+		Avatar.speak(answer, client, function() {
+			Avatar.Speech.end(client);
+		});
+
+	});
+	
+}
+
+
+
+function getJsonRpc(value) {
+	
+	var jsonrpc = {};
+	jsonrpc.id = value;
+	jsonrpc.params = {};
+	jsonrpc.params.apikey = _JeedomConf.apikey;
+	jsonrpc.params.plugin = 'sarah';
+	jsonrpc.jsonrpc = '2.0';
+	
+	return jsonrpc;
+}
+
+
+
+function sendJsonRequest(_jsonrpc, callback){
+	
+	var uri = _JeedomConf.ip + _JeedomConf.pathJeedomApi;
+	
+	info('uri:', uri.yellow);
+	info('rpc:', _jsonrpc);
+	
+	request({
+		url: uri,
+		method: 'POST',
+		form: {request: JSON.stringify(_jsonrpc)}
+	},
+	function (err, response, json) {
+		if (err || response.statusCode != 200) {
+			info('Error: Callback request'.red);
+			return callback(false);
+		}
+		
+		callback(true);
+	   
+	});
+	
+}
+
